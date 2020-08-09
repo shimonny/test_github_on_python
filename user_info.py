dd2a83ef5fde69dba9223ff24649f328c20f04cb
shimonaya <nanishen656@gmail.com>
2020-08-09 15:43:19 +0900

    add ability attribute

--- user_info.py
+++ user_info.py
@@ -1,20 +1,21 @@
 class Dog:
-    def __init__(self, name):
+    def __init__(self, name, avility):
         self.name = name
+        self.abil = avility
 
     def greet(self):
-        return f"My name is {self.name}"
+        return f"My name is {self.name}, I have {self.abil} skill!"
 
 
 class Cat(Dog):
-    def __init__(self, name):
-        super().__init__(name)
+    def __init__(self, name, ability):
+        super().__init__(name, ability)
 
     def hello(self):
         print(self.greet())
 
 
-cat = Cat("John")
+cat = Cat("Taro", "fly")
 cat.hello()
 
 
