58af5081ccd0e56ec666cf53263f2dfecb44f284
shimonaya <nanishen656@gmail.com>
2020-08-09 11:17:42 +0900

    class

--- user_info.py
+++ user_info.py
@@ -1,1 +1,18 @@
-print("I could learn about asyncio from this project")
+class Dog:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        return f"My name is {self.name}"
+
+
+class Cat(Dog):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def hello(self):
+        print(self.greet())
+
+
+cat = Cat("John")
+cat.hello()
